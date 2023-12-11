55eeb6114081691953155ef8a9176120bc071d28
BadWolf22 <matthewpostel02@gmail.com>
2023-12-11 01:47:19 -0600

    Day10 p2 (not quite done)

--- 2023/Day10/index.js
+++ 2023/Day10/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 let input;
 
 function formatFile() {
-    input = fs.readFileSync("sample2.txt", { encoding: 'utf8', flag: 'r' });
+    input = fs.readFileSync("input.txt", { encoding: 'utf8', flag: 'r' });
     input = input.replaceAll("\r", "").split("\n");
 }
 
@@ -21,7 +21,7 @@ function part1() {
             }
         }
     }
-    return Math.max(...Object.values(tiles).map(e => e.distance).filter(e => e!=undefined));
+    return Math.max(...Object.values(tiles).map(e => e.distance).filter(e => e != undefined));
 }
 
 function connectTiles() {
@@ -35,33 +35,33 @@ function connectTiles() {
                     connected.shape = ".";
                     break;
                 } case "|": {
-                    connected.push(`${x},${y-1}`);
-                    connected.push(`${x},${y+1}`);
+                    connected.push(`${x},${y - 1}`);
+                    connected.push(`${x},${y + 1}`);
                     connected.shape = "|";
                     break;
                 } case "-": {
-                    connected.push(`${x-1},${y}`);
-                    connected.push(`${x+1},${y}`);
+                    connected.push(`${x - 1},${y}`);
+                    connected.push(`${x + 1},${y}`);
                     connected.shape = "-";
                     break;
                 } case "L": {
-                    connected.push(`${x},${y-1}`);
-                    connected.push(`${x+1},${y}`);
+                    connected.push(`${x},${y - 1}`);
+                    connected.push(`${x + 1},${y}`);
                     connected.shape = "L";
                     break;
                 } case "J": {
-                    connected.push(`${x},${y-1}`);
-                    connected.push(`${x-1},${y}`);
+                    connected.push(`${x},${y - 1}`);
+                    connected.push(`${x - 1},${y}`);
                     connected.shape = "J";
                     break;
                 } case "7": {
-                    connected.push(`${x-1},${y}`);
-                    connected.push(`${x},${y+1}`);
+                    connected.push(`${x - 1},${y}`);
+                    connected.push(`${x},${y + 1}`);
                     connected.shape = "7";
                     break;
                 } case "F": {
-                    connected.push(`${x+1},${y}`);
-                    connected.push(`${x},${y+1}`);
+                    connected.push(`${x + 1},${y}`);
+                    connected.push(`${x},${y + 1}`);
                     connected.shape = "F";
                     break;
                 } case "S": {
@@ -82,9 +82,79 @@ function connectTiles() {
     return [starting, tiles];
 }
 
+const NORMALS = {
+    "L": "NE",
+    "F": "SE",
+    "7": "SW",
+    "J": "NW"
+}
+
 function part2() {
     formatFile();
     let [starting, tiles] = connectTiles();
+    let q = [starting];
+
+    // Decide the normal of "S" because shape is unknown
+    let [x, y] = starting.split(",");
+    let dir = "";
+    dir += tiles[starting].includes(`${x},${y + 1}`) ? "N" : "S";
+    dir += tiles[starting].includes(`${x + 1},${y}`) ? "W" : "E";
+    tiles[starting].surfaceNorm = dir;
+
+    while (q.length != 0) {
+        const curr = q.pop();
+        if (tiles[curr].shape && NORMALS[tiles[curr].shape]) tiles[curr].surfaceNorm = NORMALS[tiles[curr].shape];
+        for (connected of tiles[curr]) {
+            if (tiles[connected].distance == undefined) {
+                q.push(connected);
+                tiles[connected].distance = tiles[curr].distance + 1;
+                if (tiles[connected]?.shape == "-") tiles[connected].surfaceNorm = tiles[curr].surfaceNorm.match(/[NS]/g)[0];
+                if (tiles[connected]?.shape == "|") tiles[connected].surfaceNorm = tiles[curr].surfaceNorm.match(/[EW]/g)[0];
+                if (curr == starting) break;
+            }
+        }
+    }
+
+    // Guess whether a normal points to the inside or not
+    tiles[starting].inside = true;
+    const surfNorms = Object.entries(tiles).filter(e => e[1].surfaceNorm != undefined).sort((a, b) => a[1].distance < b[1].distance ? -1 : 1);
+    for (let i = 1; i < surfNorms.length; i++) {
+        if (surfNorms[i][1].surfaceNorm.match(new RegExp(`[${surfNorms[i - 1][1].surfaceNorm}]`, 'g')) == null) surfNorms[i][1].inside = !surfNorms[i - 1][1].inside;
+        else surfNorms[i][1].inside = surfNorms[i - 1][1].inside;
+    }
+
+    // Decide whether .inside actually points to the inside lol
+    if (surfNorms.map(e => e[1].inside).filter(e => e == true).length < surfNorms.length / 2) {
+        surfNorms.forEach(e => e[1].inside = !e[1].inside);
+    }
+
+    // This gives the right number for the samples, but not the input
+
+    let numInside = 0;
+    // Find the closest surfaceNorm
+    for (let [y, row] of input.entries()) {
+        for (let [x, char] of row.split("").entries()) {
+            if (tiles[[x, y]].distance != undefined) continue;
+            // This is very slow lol
+            let normsByDist = surfNorms.map(norm => {
+                let [normX, normY] = norm[0].split(",");
+                let distance = Math.abs(normX - x) + Math.abs(normY - y);
+                return { distance, inside: norm[1].inside, coord: norm[0], surfaceNorm: norm[1].surfaceNorm };
+            }).sort((a, b) => a.distance < b.distance ? -1 : 1);
+            let closest = normsByDist[0];
+            let [origX, origY] = closest.coord.split(",").map(e => parseInt(e));
+            let comingFrom = "";
+            if (origY - y > 0) comingFrom += "N";
+            if (origY - y < 0) comingFrom += "S";
+            if (origX - x > 0) comingFrom += "W";
+            if (origX - x < 0) comingFrom += "E";
+            let dirMatch = comingFrom.match(new RegExp(`[${closest.surfaceNorm}]`, 'g'));
+            if ((dirMatch != null && closest.inside) || dirMatch == null && !closest.inside) {
+                numInside++;
+            }
+        }
+    }
+    return numInside;
 }
 
 console.log(part1(), part2());
