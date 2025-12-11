158055fb443dba0d4867d5bb394dbb4f063ce1b2
BadWolf22 <matthewpostel02@gmail.com>
2025-12-11 00:44:19 -0600

    Day3

---
+++ 2025/Day3/Program.cs
@@ -0,0 +1,42 @@
+﻿class Program {
+    static void Main() {
+        Console.WriteLine($"Part1 Sample: {Part1("sample.txt")}");
+        Console.WriteLine($"Part1 Input: {Part1("input.txt")}");
+        Console.WriteLine($"Part2 Sample: {Part2("sample.txt")}");
+        Console.WriteLine($"Part2 Input: {Part2("input.txt")}");
+    }
+
+    static int Part1(string fileName) {
+        var joltage = 0;
+
+        var banks = File.ReadAllLines(fileName);
+        foreach (var bank in banks)
+        {
+            var firstNum = bank[..^1].MaxBy(x => int.Parse($"{x}"));
+            var lastNum = bank[(bank.IndexOf(firstNum) + 1)..].MaxBy(x => int.Parse($"{x}"));
+            joltage += int.Parse($"{firstNum}{lastNum}");
+        }
+        return joltage;
+    }
+
+    static double Part2(string fileName) {
+        double joltage = 0;
+        const int digitsToFind = 12;
+
+        var banks = File.ReadAllLines(fileName);
+        foreach (var bank in banks)
+        {
+            var joltageString = "";
+            var lastIndex = -1;
+            while (joltageString.Length < digitsToFind)
+            {
+                var searchRange = bank[(lastIndex + 1)..^(digitsToFind-(1+joltageString.Length))];
+                var maxDigit = searchRange.MaxBy(x => int.Parse($"{x}"));
+                joltageString += maxDigit;
+                lastIndex += searchRange.IndexOf(maxDigit) + 1;
+            }
+            joltage += double.Parse(joltageString);
+        }
+        return joltage;
+    }
+}
